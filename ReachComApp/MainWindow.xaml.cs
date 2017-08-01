fa07b691e43f5bfcc0d9efaa62f726c65c884bc1
StevenUlbrich <StevenUlbrich@me.com>
2017-07-31 22:27:02 -0500

    July 31

--- ReachComApp/MainWindow.xaml.cs
+++ ReachComApp/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using ReachComData;
 
 namespace ReachComApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -14,10 +17,8 @@ namespace ReachComApp
         }
 
 
-
         private void TabControlMain_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
         private void ButoonSealAppConfirm_Click(object sender, RoutedEventArgs e)
@@ -25,9 +26,7 @@ namespace ReachComApp
             if (TabControlMain.SelectedIndex == TabControlMain.Items.Count - 1)
                 TabControlMain.SelectedIndex = 0;
             else
-            {
                 TabControlMain.SelectedIndex++;
-            }
         }
 
         private void ButoonErrorConfirm_Click(object sender, RoutedEventArgs e)
@@ -35,10 +34,7 @@ namespace ReachComApp
             if (TabControlMain.SelectedIndex == TabControlMain.Items.Count - 1)
                 TabControlMain.SelectedIndex = 0;
             else
-            {
                 TabControlMain.SelectedIndex++;
-            }
-
         }
 
         private void ButoonCurrentActionConfirm_Click(object sender, RoutedEventArgs e)
@@ -46,10 +42,7 @@ namespace ReachComApp
             if (TabControlMain.SelectedIndex == TabControlMain.Items.Count - 1)
                 TabControlMain.SelectedIndex = 0;
             else
-            {
                 TabControlMain.SelectedIndex++;
-            }
-
         }
 
         private void ButoonCurrentImpactsConfirm_Click(object sender, RoutedEventArgs e)
@@ -57,9 +50,7 @@ namespace ReachComApp
             if (TabControlMain.SelectedIndex == TabControlMain.Items.Count - 1)
                 TabControlMain.SelectedIndex = 0;
             else
-            {
                 TabControlMain.SelectedIndex++;
-            }
         }
 
         private void ButtonIntro_Click(object sender, RoutedEventArgs e)
@@ -67,9 +58,35 @@ namespace ReachComApp
             if (TabControlMain.SelectedIndex == TabControlMain.Items.Count - 1)
                 TabControlMain.SelectedIndex = 0;
             else
-            {
                 TabControlMain.SelectedIndex++;
-            }
+        }
+
+        private void ButonReset_Click(object sender, RoutedEventArgs e)
+        {
+            TabControlMain.SelectedIndex = 1;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Stream stream = File.Open(".\\ExcelData\\REACH.xlsx", FileMode.Open);
+
+            var data = new DataLayer();
+
+            var sealAppViewSource = (CollectionViewSource)FindResource("sealAppViewSource");
+            // Load data by setting the CollectionViewSource.Source property:
+            sealAppViewSource.Source = data.GetSealApplications(stream);
+            System.Windows.Data.CollectionViewSource errorLookupViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("errorLookupViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            errorLookupViewSource.Source = data.GetErrorLookups(stream);
+
+            System.Windows.Data.CollectionViewSource currentActionViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("currentActionViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            currentActionViewSource.Source = data.GetCurrentActions(stream);
+        }
+
+        private void errorLookupDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
